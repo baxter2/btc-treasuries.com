dac58eb82192b04c408d300853a134797c67897b
baxter2 <baxcallan@gmail.com>
2022-12-31 20:42:17 +1100

    $ bin/importmap pin chart.js
    $ bin/importmap pin chartjs-adapter-moment

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,11 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# $ bin/importmap pin chart.js
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.1.1/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.1/dist/color.esm.js"
+
+# $ bin/importmap pin chartjs-adapter-moment
+pin "chartjs-adapter-moment", to: "https://ga.jspm.io/npm:chartjs-adapter-moment@1.0.1/dist/chartjs-adapter-moment.esm.js"
+pin "moment", to: "https://ga.jspm.io/npm:moment@2.29.4/moment.js"
