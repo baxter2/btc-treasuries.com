a5736d98cb328a10b3360f506183b71af045c776
baxter2 <baxcallan@gmail.com>
2023-01-01 09:41:36 +1100

    Round total_btc of an entity.
    Add the countries show page so can see all of the transactions an entity has made so far, visualise it on Chart.js and display some basic stats.

--- app/controllers/treasuries/countries_controller.rb
+++ app/controllers/treasuries/countries_controller.rb
@@ -3,4 +3,10 @@ class Treasuries::CountriesController < ApplicationController
     view_object = Treasuries::Entities.new("Country").call
     render locals: view_object.locals
   end
+
+  def show
+    @country = Country.find_by_permalink(params[:permalink])
+    @transactions = @country.transactions
+    @chart_data = Treasuries::ChartDataQuery.new(@transactions).group_by_individual
+  end
 end
