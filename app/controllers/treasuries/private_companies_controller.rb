54d685526fc52483301aaac8cfaf54ea04092dae
baxter2 <baxcallan@gmail.com>
2023-01-03 00:41:40 +1100

    Fix typos

--- app/controllers/treasuries/private_companies_controller.rb
+++ app/controllers/treasuries/private_companies_controller.rb
@@ -27,7 +27,7 @@ class Treasuries::PrivateCompaniesController < ApplicationController
           <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
           </svg>
-          <a href="/treasuries/countries/#{@private_company.permalink}" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">#{@private_company.name}</a>
+          <a href="/treasuries/private_companies/#{@private_company.permalink}" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">#{@private_company.name}</a>
         </div>
       </li>
     ).html_safe
--- app/controllers/treasuries/public_companies_controller.rb
+++ app/controllers/treasuries/public_companies_controller.rb
@@ -27,7 +27,7 @@ class Treasuries::PublicCompaniesController < ApplicationController
           <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
           </svg>
-          <a href="/treasuries/countries/#{@public_company.name}" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">#{@public_company.name} (#{@public_company.long_ticker})</a>
+          <a href="/treasuries/public_companies/#{@public_company.permalink}" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">#{@public_company.name} (#{@public_company.long_ticker})</a>
         </div>
       </li>
     ).html_safe
